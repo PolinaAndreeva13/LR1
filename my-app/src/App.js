831955ff7d2c31a205aba655a597dc5fc704acdc
Champinbon <wowchik_3708@mail.ru>
2023-03-10 00:27:45 +0700

    опять поломка

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,37 @@
-import './App.css';//D:\programming\github\LR1\my-app
+//D:\programming\github\LR1\my-app
+import './head/head.css';
+import './body/body.css';
+import './body/body.css';
 import Ccomponent from "./Code";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div>
 
-          <Ccomponent/>
-        </a>
+      <header className='head'>
+
+        Рассчет индекса массы тела без мам, пап, врачей и денег!!!
+
       </header>
+
+      <div className="body">
+
+        <div className='standart_text' > 
+
+        <p>
+          Чтобы ответить на вопрос, нормальный вес у вас или избыточный, необходимо научиться правильно его оценивать.
+          Для этого нужно, в первую очередь, рассчитать индекс массы тела (ИМТ).
+        </p>
+
+        <p>
+          Заполните поля ниже:
+        </p>
+
+        </div>
+
+
+      </div>
+
     </div>
   );
 }
