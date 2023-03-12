35046e4e8128ebcf6f122903b81fe243f6afdce9
Champinbon <wowchik_3708@mail.ru>
2023-03-12 22:13:59 +0700

    old

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 //D:\programming\github\LR1\my-app
 import './head/head.css';
 import './body/body.css';
-import './body/body.css';
+import formula from '/body/BMI_formula_picture.png"';
+
 import Ccomponent from "./Code";
 
 function App() {
@@ -18,17 +19,36 @@ function App() {
 
         <div className='standart_text' > 
 
-        <p>
-          Чтобы ответить на вопрос, нормальный вес у вас или избыточный, необходимо научиться правильно его оценивать.
-          Для этого нужно, в первую очередь, рассчитать индекс массы тела (ИМТ).
-        </p>
+          <p>
+            Чтобы ответить на вопрос, нормальный вес у вас или избыточный, необходимо научиться правильно его оценивать.
+            Для этого нужно, в первую очередь, рассчитать индекс массы тела (ИМТ).
+          </p>
 
-        <p>
-          Заполните поля ниже:
-        </p>
+          <p>
+            Заполните поля ниже:
+          </p>
 
         </div>
 
+        <div className='calc_style'>
+          <p>
+          <Ccomponent/>
+          </p>
+        </div>
+        
+        <div className='reference_information_style'>
+          <h1>
+          Справочная информация
+          </h1>
+
+        </div>
+
+        <div className='standart_text'>
+
+        <p>Индекс массы тела рассчитывается по формуле:</p>
+          <img src='formula'/>
+
+        </div>
 
       </div>
 
